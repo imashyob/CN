9f3126b02b26b8a8c5c20ef08738637fdc277748
imashyob <imashyob69@gmail.com>
2024-12-10 23:38:29 +0700

    Initial commit

---
+++ articles/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import Article
+
+def article_list(request):
+    articles = Article.objects.all()
+    return render(request, 'articles/article_list.html', {'articles': articles})
